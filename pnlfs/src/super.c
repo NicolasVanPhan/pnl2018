372b399af288d393874f203310fed2e7cb9ceffd
Nicolas Phan <nicolas.van.phan@gmail.com>
2018-05-18 01:17:02 +0200

    Extract superblock info

--- pnlfs/src/super.c
+++ pnlfs/src/super.c
@@ -9,12 +9,12 @@
 #include <linux/module.h>
 //#include <linux/string.h>
 #include <linux/fs.h>
-//#include <linux/slab.h>
+#include <linux/slab.h>
 #include <linux/init.h>
 //#include <linux/blkdev.h>
 //#include <linux/parser.h>
 //#include <linux/random.h>
-//#include <linux/buffer_head.h>
+#include <linux/buffer_head.h>
 //#include <linux/exportfs.h>
 #include <linux/vfs.h>
 //#include <linux/seq_file.h>
@@ -34,10 +34,42 @@ MODULE_LICENSE("GPL");
 /* -------------------------------------------------------------------------- */
 
 /*
- * This function is needed by pnlfs_mount(), it setups the super block
+ * This function is needed by pnlfs_mount(), it reads the pnlfs disk superblock
+ * and fills the VFS superblock variable consequently
  */
 static int pnlfs_fill_super(struct super_block *sb, void *data, int silent)
 {
+	struct buffer_head	*bh;
+	struct pnlfs_superblock	*psb;
+	struct pnlfs_sb_info	*psbi;
+
+	/* Initialize the super block given as a parameter */
+	sb->s_magic = PNLFS_MAGIC;
+	sb->s_blocksize = PNLFS_BLOCK_SIZE;
+	sb->s_maxbytes = PNLFS_MAX_FILESIZE;
+
+	/* Reading the superblock */
+	bh = sb_bread(sb, 0); // Reading the disk.img superblock
+	if (bh == NULL) {
+		pr_err("pnlfs: The superblock is unreadable\n");
+		return -1;
+	}
+
+	/* Extracting superblock content */
+	psb = (struct pnlfs_superblock *)bh->b_data;
+	psbi = kmalloc(sizeof(struct pnlfs_sb_info), GFP_KERNEL);
+	psbi->nr_blocks = le32_to_cpu(psb->nr_blocks);
+	psbi->nr_inodes = le32_to_cpu(psb->nr_inodes);
+	psbi->nr_istore_blocks = le32_to_cpu(psb->nr_istore_blocks);
+	psbi->nr_ifree_blocks = le32_to_cpu(psb->nr_ifree_blocks);
+	psbi->nr_bfree_blocks = le32_to_cpu(psb->nr_bfree_blocks);
+	psbi->nr_free_inodes = le32_to_cpu(psb->nr_free_inodes);
+	psbi->nr_free_blocks = le32_to_cpu(psb->nr_free_blocks);
+
+	/* Attaching the extracted content into the generic structure sb */
+	sb->s_fs_info = (void *)psbi;
+
+	brelse(bh);
 	return -1;
 }
 
