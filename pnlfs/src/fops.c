1ca0c5463063636befba7a4be32270b7fbda31e5
Nicolas Phan <nicolas.van.phan@gmail.com>
2018-05-23 11:06:19 +0200

    Fix ls

--- pnlfs/src/fops.c
+++ pnlfs/src/fops.c
@@ -24,27 +24,33 @@ static int pnlfs_iterate_shared(struct file *fdir, struct dir_context *ctx)
 	unsigned char		d_type;
 	int			i;
 
-	/* Add the .. directory */
-	if (!dir_emit_dots(fdir, ctx))
+	if (ctx->pos)
 		return 0;
 
 	/* Get the block where our directory's filelist is stored */
 	idir = file_inode(fdir);
 	pnli = container_of(idir, struct pnlfs_inode_info,  vfs_inode);
+	if (pnli == NULL)
+		return 0;
 	nr_entries = pnli->nr_entries;
 	blkno = pnli->index_block;
 	bh = sb_bread(idir->i_sb, blkno);
 
+	/* Add the . and .. directories */
+	if (!dir_emit_dots(fdir, ctx))
+		return 0;
+
 	/* Iterate over our filelist */
 	rows = (struct pnlfs_file *)bh->b_data;
-	i = ctx->pos - 2;
-	if (i < nr_entries) {
+	for (i = 0; i < PNLFS_MAX_DIR_ENTRIES; i++) {
 		name = rows[i].filename;
 		namelen = strnlen(name, PNLFS_FILENAME_LEN);
+		if (!namelen)
+			continue;
 		ino = rows[i].inode;
 		d_type = DT_UNKNOWN;
 		if(dir_emit(ctx, name, namelen, ino, d_type))
-			ctx->pos++;
+			ctx->pos += sizeof(struct pnlfs_file);
 	}
 
 	brelse(bh);
