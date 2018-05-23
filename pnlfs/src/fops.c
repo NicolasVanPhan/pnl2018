d1c1486e7c97c51b316ac69fdfadb9bfbb44f84d
Nicolas Phan <nicolas.van.phan@gmail.com>
2018-05-23 21:04:23 +0200

    ADD PNLFS READ (OMG)

--- pnlfs/src/fops.c
+++ pnlfs/src/fops.c
@@ -127,18 +127,62 @@ pnlfs_write(struct file* file, const char __user *src, size_t len, loff_t *off)
 
 		mark_buffer_dirty(bh);
 		brelse(bh);
-	} while (*off < len);
+	} while (bno != blklast);
 	return len;
 }
 
 static ssize_t
-pnlfs_read(struct file *file, char __user *dest, size_t len, loff_t *off)
+pnlfs_read(struct file* file, char __user *dest, size_t len, loff_t *off)
 {
-	return -1;
+	struct inode		*inode;
+	struct buffer_head	*bh;
+	int			nb_char_per_blk;
+	int			bno;
+	const char		*from;
+	char			*to;
+	int			length;
+
+	inode = file->f_inode;
+	nb_char_per_blk = PNLFS_BLOCK_SIZE;
+
+	if (*off < 0)
+		return 0;
+
+	/* Get the right data block according to current position */
+	bno = pnlfs_findex_blk(inode, *off / PNLFS_BLOCK_SIZE);
+
+	/* If cursor is already beyond file end, quit */
+	if (bno < 0)
+		return 0;
+
+	length = nb_char_per_blk - *off % nb_char_per_blk;
+	length = len < length ? len : length;
+
+	/* Open the block for reading */
+	bh = sb_bread(inode->i_sb, bno);
+	if (bh == NULL) {
+		pr_err("pnlfs_read() : Cannot read block %d\n", bno);
+		return -1;
+	}
+
+	/* Read ! */
+	to = dest;
+	from = &((char*)bh->b_data)[*off];
+	if (copy_to_user(to, from, length)) {
+		pr_err("pnlfs_read() : copy to user failed\n");
+		return -EIO;
+	}
+
+	/* Update cursor position */
+	*off += length;
+
+	brelse(bh);
+	return length;
 }
 
 struct file_operations pnlfs_file_operations = {
 	.iterate_shared = pnlfs_iterate_shared,
 	.write		= pnlfs_write,
+	.read		= pnlfs_read,
 };
 
